eb6ba009fee0197ad6a1bf0e35c605fcb60c5813
vijaykamalr <vijaykamalr@gmail.com>
2022-12-17 18:38:16 +0000

    varible annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,19 @@
+let numberType = 5;
+let stringType: string = 'test';
+let nullType: null = null;
+let undefinedType: undefined = undefined;
+//builtin
+let builtInDateType: Date = new Date();
+
+//Array
+let arrayType: number[] = [1,2,3]
+
+//classes
+class Car{}
+let classType: Car = new Car();
+
+//object
+let objectType:{x: number; y: number} = {
+    x: 10, 
+    y: 20
+}
